8d71dab829c45ec2d99597857bb1c815b05f7fa3
antlbn <lubnyanton@gmail.com>
2026-06-14 13:36:02 +0200

    feat: M6 deploy with  Github

--- src/api/loadData.ts
+++ src/api/loadData.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 import { StudentSchema, type Student } from '../types/student'
 
 export async function loadStudents(): Promise<Student[]> {
-  const response = await axios.get<unknown[]>('/eda_preprocessed.json')
+  const response = await axios.get<unknown[]>(import.meta.env.BASE_URL + 'eda_preprocessed.json')
   const result = z.array(StudentSchema).safeParse(response.data)
 
   if (!result.success) {
